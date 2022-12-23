bd3b95f0a7c165deafa8e22b4112802cbd8b4f25
Pasquale De Lucia <pasquale.delucia96@gmail.com>
2022-12-23 16:34:04 +0100

    Fix cross origin images

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // Install express server
-const express = require('express');
-const path = require('path');
-const helmet = require("helmet");
+import express from 'express';
+import path from 'path';
+import helmet from "helmet";
+import fetch from 'node-fetch';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -14,9 +21,23 @@ app.use(helmet(
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/rick-and-morty'));
 
-app.get('/*', function(req,res) {
+const createBufferFromImage = async (url) => {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const arrayBuffer = await blob.arrayBuffer();
+  return Buffer.from(arrayBuffer);
+  // await fs.writeFile(path, buffer);
+}
 
-res.sendFile(path.join(__dirname+'/dist/rick-and-morty/index.html'));
+app.get('/image', async function(req,res) {
+  // const imagePathSplitted = req.query.image.split('/')
+  const buffer = await createBufferFromImage(req.query.image);
+  res.end(buffer, 'binary');
+  // res.sendFile(req.query.image);
+});
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/rick-and-morty/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
